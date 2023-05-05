67af26a5e226e0bae768fced52d7a5e11e0865c1
JaRodriguez97 <cristi542012@gmail.com>
2023-05-05 16:20:27 -0500

    landing page en plantilla funcional listo para ingresar informacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,10 +17,9 @@ import {
   faBars,
   faHeart,
   faShoppingCart,
+   
   faUser,
 } from '@fortawesome/free-solid-svg-icons';
-// import { userInterface } from '@models/users.interface';
-// import { UsersService } from '@service/Users/users.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import Swal from 'sweetalert2';
 
@@ -35,17 +34,9 @@ export class AppComponent implements OnInit {
   faShoppingCart: IconDefinition = faShoppingCart;
   faUser: IconDefinition = faUser;
   faBars: IconDefinition = faBars;
-  // pedidos!: Array<productInterface>;
-  // user!: userInterface | undefined;
-  // userID!: string | null | undefined;
-  // products!: productInterface[];
   paragraphSpinner!: string;
 
   // !!! Sections !!!
-
-  @ViewChild('favorites') favorites!: ElementRef;
-  @ViewChild('order') order!: ElementRef;
-  @ViewChild('userInfo') userInfo!: ElementRef;
 
   @ViewChild('header') header!: ElementRef;
   @ViewChild('home') home!: ElementRef;
@@ -60,10 +51,9 @@ export class AppComponent implements OnInit {
     private readonly formBuilder: FormBuilder,
     @Inject(DOCUMENT) private document: Document,
     private renderer: Renderer2,
-    // private usersService: UsersService,
     public router: Router,
     public activatedRoute: ActivatedRoute,
-    public spinner: NgxSpinnerService // public productsServices: ProductsService
+    public spinner: NgxSpinnerService
   ) {}
 
   @HostListener('window:scroll')
@@ -77,31 +67,12 @@ export class AppComponent implements OnInit {
     this.paragraphSpinner = 'Cargando...';
     this.spinner
       .show()
-      .then(() => {
-        // this.userID = localStorage.getItem('userID');
-        // if (this.userID){}
-        //   // this.usersService.getUser(this.userID).subscribe(
-        //   //   (res) => {
-        //   //     this.user = res ? res : undefined;
-        //   //     this.pedidos =
-        //   //       this.user && this.user.pedido && this.user.pedido.length
-        //   //         ? this.user.pedido
-        //   //         : this.pedidos;
-        //   //   },
-        //   //   (err) => console.error(err),
-        //   //   () => this.spinner.hide()
-        //   // );
-        // else {
-        // this.pedidos = JSON.parse(localStorage.getItem('pedido')!);
-        // }
-      })
-      .finally(() =>
-        setTimeout(() => {
-          if (this.document.location.pathname !== '/') this.getOutSections();
-          else this.closeSection();
-          this.spinner.hide();
-        }, 1000)
-      );
+      .then(() =>
+        this.document.location.pathname !== '/'
+          ? this.getOutSections()
+          : this.closeSection()
+      )
+      .finally(() => this.spinner.hide());
   }
 
   async getOutSections() {
@@ -130,218 +101,4 @@ export class AppComponent implements OnInit {
 
     this.router.navigate(['/']);
   }
-
-  // getOrder() {
-  //   if (this.pedidos && this.pedidos.length)
-  //     this.ngOnInit().then(() =>
-  //       this.renderer.addClass(this.order.nativeElement, 'active')
-  //     );
-  //   else
-  //     Swal.fire({
-  //       icon: 'warning',
-  //       html: '<span>No has agregado nada al carro de compras</span>',
-  //     })
-  //       .then(() => this.ngOnInit())
-  //       .finally(() => this.spinner.hide());
-  // }
-
-  // getOutOrder() {
-  //   this.renderer.removeClass(this.order.nativeElement, 'active');
-  //   // this.closeSection();
-  // }
-
-  getOutUser() {
-    this.renderer.removeClass(this.userInfo.nativeElement, 'active');
-    this.closeSection();
-  }
-
-  // getUser() {
-  //   if (this.user)
-  //     return this.renderer.addClass(this.userInfo.nativeElement, 'active');
-
-  //   this.router.navigate(['login']).then(() => this.ngOnInit());
-  // }
-
-  // logOut() {
-  //   Swal.fire({
-  //     icon: 'success',
-  //     imageWidth: 100,
-  //     confirmButtonColor: '#000',
-  //     confirmButtonAriaLabel: '',
-  //     html: '<b>Sesión Cerrada Exitosamente</b>',
-  //     scrollbarPadding: false,
-  //   })
-  //     .then(() => this.ngOnInit())
-  //     .then(() => this.getOutUser())
-  //     .then(() => this.router.navigate(['/login']))
-  //     .then(() => (this.pedidos = []))
-  //     .then(() => (this.user = undefined))
-  //     .then(() => (this.userID = undefined))
-  //     .then(() => localStorage.clear());
-  // }
-
-  // async reloadTo(uri: string) {
-  //   this.router
-  //     .navigateByUrl('/', { skipLocationChange: true })
-  //     .then(() => this.router.navigate([uri]));
-  // }
-
-  // async getFavorite() {
-  //   if (!this.user)
-  //     return Swal.fire({
-  //       icon: 'warning',
-  //       html: '<span>Por favor inicia sesión primero para enlistar los productos favoritos</span>',
-  //     });
-
-  //   if (this.user.favoritos && this.user.favoritos.length)
-  //     return this.ngOnInit().then(() =>
-  //       this.renderer.addClass(this.favorites.nativeElement, 'active')
-  //     );
-
-  //   return Swal.fire({
-  //     icon: 'warning',
-  //     html: '<span>No has marcado ningun producto como favorito</span>',
-  //   })
-  //     .then(() => this.ngOnInit())
-  //     .finally(() => this.spinner.hide());
-  // }
-
-  // validateFavorite(_id: String): Boolean {
-  //   if (this.user && this.user.favoritos && this.user.favoritos.length)
-  //     return this.user.favoritos.some((favorito) => favorito === _id);
-
-  //   return false;
-  // }
-
-  // addFavorite(_id: string) {
-  //   this.spinner.show().then(() => {
-  //     if (this.user) {
-  //       if (this.user.favoritos) {
-  //         if (this.user.favoritos.length) {
-  //           let index = this.user.favoritos.indexOf(_id);
-
-  //           if (index == -1) this.user.favoritos.push(_id);
-  //           else this.user.favoritos!.splice(index, 1);
-  //         } else this.user.favoritos.push(_id);
-  //       } else {
-  //         this.user.favoritos = [];
-  //         this.user.favoritos.push(_id);
-  //       }
-
-  //       this.usersService
-  //         .updateUser(this.user._id!, this.user.favoritos, 'favoritos')
-  //         .subscribe(
-  //           (res) => this.ngOnInit(),
-  //           (err) =>
-  //             this.spinner.hide().then(() => {
-  //               console.error(err);
-  //               Swal.fire({
-  //                 confirmButtonColor: '#000',
-  //                 icon: 'error',
-  //                 html: err.error.message,
-  //                 scrollbarPadding: false,
-  //               });
-  //             }),
-  //           () => this.spinner.hide()
-  //         );
-  //     } else {
-  //       this.spinner.hide();
-  //       Swal.fire({
-  //         icon: 'warning',
-  //         title: 'NO HAS INICIADO SESIÓN',
-  //         text: 'Registrate antes para poder marcar como favorito',
-  //         showCancelButton: true,
-  //         scrollbarPadding: false,
-  //       }).then((response) => {
-  //         if (response.value) this.router.navigate(['/login']);
-  //       });
-  //     }
-  //   });
-  // }
-
-  // existeComboPedido(_id: string): Boolean {
-  //   if (this.pedidos && this.pedidos.length)
-  //     return this.pedidos?.some((product) => product._id === _id)!;
-  //   else if (this.user)
-  //     return this.user.pedido?.some((product) => product._id === _id)!;
-
-  //   return false;
-  // }
-
-  // async addToCar(product: productInterface, i?: number): Promise<void> {
-  //   if (typeof i == 'number') {
-  //     let list = this.document.querySelectorAll('.iconsList')[i];
-
-  //     this.renderer.addClass(list, 'active');
-  //   }
-
-  //   if (!this.user) {
-  //     if (!this.pedidos) {
-  //       this.spinner.hide().then(() => {
-  //         Swal.fire({
-  //           icon: 'question',
-  //           title: 'NO ESTÁ REGISTRADO',
-  //           html: `<h4>Desea ingresar antes de hacer su pedido?</h4>
-  //             <h6 style="font-size:10px">Si marca NO podrá hacer su pedido sin
-  //             ningún problema, pero de manera anónima.</h6>`,
-  //           showCancelButton: true,
-  //           cancelButtonText: 'Quiero hacer mi pedido ya',
-  //           confirmButtonText: 'Deseo ingresar a mi cuenta',
-  //           scrollbarPadding: false,
-  //         }).then((response) => {
-  //           if (response.isConfirmed) {
-  //             this.router.navigate(['/login', product._id]);
-  //             return;
-  //           }
-
-  //           this.pedidos = [];
-  //           this.pedidos.push(product);
-  //           localStorage.setItem('pedido', JSON.stringify(this.pedidos));
-  //         });
-  //       });
-  //     } else {
-  //       this.pedidos.push(product);
-  //       localStorage.setItem('pedido', JSON.stringify(this.pedidos));
-  //       this.ngOnInit().then(() => setTimeout(() => this.spinner.hide(), 500));
-  //     }
-  //   } else {
-  //     if (!this.pedidos) this.pedidos = [];
-
-  //     this.pedidos.push(product);
-
-  //     this.usersService
-  //       .updateUser(this.userID!, this.pedidos, 'pedido')
-  //       .subscribe(
-  //         () => this.ngOnInit(),
-  //         (err) => console.error(err),
-  //         () => this.spinner.hide()
-  //       );
-  //   }
-  // }
-
-  // async restToCar(_id: string, i?: number) {
-  //   this.spinner.show().then(() => {
-  //     if (typeof i == 'number') {
-  //       let list = this.document.querySelectorAll('.iconsList')[i];
-
-  //       this.renderer.removeClass(list, 'active');
-  //     }
-
-  //     if (this.user) {
-  //       this.pedidos = this.user.pedido?.filter(
-  //         (product) => product._id !== _id
-  //       )!;
-
-  //       this.usersService
-  //         .updateUser(this.userID!, this.pedidos, 'pedido')
-  //         .subscribe(() => this.spinner.hide());
-  //     } else {
-  //       this.pedidos = this.pedidos.filter((product) => product._id !== _id);
-
-  //       localStorage.setItem('pedido', JSON.stringify(this.pedidos));
-
-  //       setTimeout(() => this.spinner.hide(), 500);
-  //     }
-  //   });
-  // }
 }
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ContactService } from '@services/Contact/contact.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -15,7 +16,8 @@ export class ContactComponent implements OnInit {
   constructor(
     private contactService: ContactService,
     private router: Router,
-    private readonly formBuilder: FormBuilder
+    private readonly formBuilder: FormBuilder,
+    private spinner: NgxSpinnerService
   ) {}
 
   ngOnInit() {
@@ -23,49 +25,52 @@ export class ContactComponent implements OnInit {
   }
 
   async sendContactMail(contactForm: FormGroup) {
-    if (contactForm.invalid)
-      Swal.fire({
-        icon: 'warning',
-        html: '<span>Por favor diligencie los campos obligatorios para poder enviar el mensaje</span>',
-        scrollbarPadding: false,
-      });
-    else
-      this.contactService.sendMesage(contactForm.value).subscribe(
-        (res) => {
-          console.log(res);
-          Swal.fire({
-            icon: 'success',
-            html: '<span>Su mensaje ha sido enviado satisfactoriamente, prontamente uno de nuestros operadores le contactará</span>',
-            // scrollbarPadding: false,
-          });
-        },
-        (err) => {
-          console.error(err);
-
-          Swal.fire({
-            icon: 'error',
-            html: '<span>Ha ocurrido un error al enviar el correo</span>',
-            // scrollbarPadding: false,
-            confirmButtonText: 'Intentar por WhatsApp',
-            confirmButtonColor: 'green',
-            showDenyButton: true,
-            denyButtonText: 'Usar app email',
-            denyButtonColor: 'black',
-          }).then((response) => {
-            if (response.isConfirmed) {
-              console.log(
-                '🚀 ~ file: contactanos.component.ts:55 ~ ContactanosComponent ~ sendContactMail ~ response.isConfirmed',
-                response.isConfirmed
-              );
-              this.router.navigateByUrl('https://wa.me/573185051107');
-            }
-            if (response.isDenied)
-              this.router.navigate([
-                `http://mailto:centralnacionaldegruasynineras@gmail.com`,
-              ]);
-          });
-        }
-      );
+    this.spinner.show().then(() => {
+      if (contactForm.invalid)
+        Swal.fire({
+          icon: 'warning',
+          html: '<span>Por favor diligencie los campos obligatorios para poder enviar el mensaje</span>',
+          scrollbarPadding: false,
+        });
+      else
+        this.contactService.sendMesage(contactForm.value).subscribe(
+          (res) => console.log(res),
+          (err) =>
+            this.spinner
+              .hide()
+              .then(() => console.error(err))
+              .then(() =>
+                Swal.fire({
+                  icon: 'error',
+                  html: '<span>Ha ocurrido un error al enviar el correo</span>',
+                  confirmButtonText: 'Intentar por WhatsApp',
+                  confirmButtonColor: 'green',
+                  showDenyButton: true,
+                  denyButtonText: 'Usar app email',
+                  denyButtonColor: 'black',
+                }).then((response) => {
+                  if (response.isConfirmed) {
+                    console.log(
+                      '🚀 ~ file: contactanos.component.ts:55 ~ ContactanosComponent ~ sendContactMail ~ response.isConfirmed',
+                      response.isConfirmed
+                    );
+                    this.router.navigateByUrl('https://wa.me/573185051107');
+                  }
+                  if (response.isDenied)
+                    this.router.navigate([
+                      `http://mailto:centralnacionaldegruasynineras@gmail.com`,
+                    ]);
+                })
+              ),
+          () =>
+            this.spinner.hide().then(() =>
+              Swal.fire({
+                icon: 'success',
+                html: '<span>Su mensaje ha sido enviado satisfactoriamente, prontamente uno de nuestros operadores le contactará</span>',
+              })
+            )
+        );
+    });
   }
 
   initForm() {
